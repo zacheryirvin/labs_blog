ecfb826dc47dd4aa6ab4cc7e9421bc7671b812f5
badCompany55 <zachirvin45@gmail.com>
2019-05-11 19:59:48 -0500

    GATSBY SETUP connect to db

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,15 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-source-pg",
+      options: {
+        connectionString:
+          "postgres://rzwmawezicnwij:66e0046b85a68d112af968d9824ae9f551b617560b30491665bf36d7182f8fc4@ec2-174-129-240-67.compute-1.amazonaws.com:5432/d7lio2fjta4l3r?ssl=true",
+        schema: "public",
+        refetchInterval: 86400,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
