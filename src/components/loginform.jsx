09eb14270a59940cd5357c5176082e37e3f78dfe
badCompany55 <zachirvin45@gmail.com>
2019-05-14 20:06:52 -0500

    window bug

--- src/components/loginform.jsx
+++ src/components/loginform.jsx
@@ -9,11 +9,10 @@ const LoginForm = () => {
   const [inputs, setInputs] = useState({ username: "", password: "" });
   const [token, setToken] = useState();
 
-  if (
-    typeof window !== undefined &&
-    token !== window.localStorage.getItem("token")
-  ) {
-    setToken(window.localStorage.getItem("token"));
+  if (typeof window !== undefined) {
+    if (token !== window.localStorage.getItem("token")) {
+      setToken(window.localStorage.getItem("token"));
+    }
   }
 
   const handleChange = e => {
